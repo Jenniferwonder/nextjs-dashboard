03f7f8b6674827709fa9841d334e59eb9c9d66e2
Jenniferwonder <Jenniferwonder@163.com>
2023-12-28 17:48:22 +0800

    Complete data mutation and error handling

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+// similar to your /create invoice page, except it imports a different form (from the edit-form.tsx file)
+// This form should be pre-populated with a defaultValue for the customer's name, invoice amount, and status
+import Form from '@/app/ui/dashboard/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/dashboard/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  if (!invoice) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
